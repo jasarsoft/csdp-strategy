3740c82a6dfe699f3a169d7831358140785ed397
Edin Jašarević <edinjasar14@gmail.com>
2021-07-30 16:03:39 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using Strategy_Pattern_First_Look.Business.Strategies.SalesTax;
+
+namespace Strategy_Pattern_First_Look
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order
+            {
+                ShippingDetails = new ShippingDetails 
+                { 
+                    OriginCountry = "Sweden",
+                    DestinationCountry = "Sweden"
+                }
+            };
+
+            var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
+            if (destination == "sweden")
+            {
+                order.SalesTaxStrategy = new SwedenSalesTaxStrategy();
+            }
+            else if (destination == "us")
+            {
+                order.SalesTaxStrategy = new USAStateSalesTaxStrategy();
+            }
+            
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+            order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
+
+            Console.WriteLine(order.GetTax());
+        }
+    }
+}
