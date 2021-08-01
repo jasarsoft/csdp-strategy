244359531e2fb1a2ebf4f99ca91e58098cff29ba
Edin Jašarević <edinjasar14@gmail.com>
2021-08-01 21:21:10 +0200

    shipping providers

---
+++ 4. shipping providers/Business/Strategies/Shipping/SwedishPostalServiceShippingStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Strategy_Pattern_Using_different_shipping_providers.Business.Models;
+using System;
+using System.Net.Http;
+
+namespace Strategy_Pattern_Using_different_shipping_providers.Business.Strategies.Shipping
+{
+    public class SwedishPostalServiceShippingStrategy : IShippingStrategy
+    {
+        public void Ship(Order order)
+        {
+            using (var client = new HttpClient())
+            {
+                Console.WriteLine("Order is shipped with PostNord");
+            }
+        }
+    }
+}
